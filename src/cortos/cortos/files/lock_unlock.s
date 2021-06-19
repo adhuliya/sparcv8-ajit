192cbc88b7212b1a1c5fee1e3e59094a7c249b44
adhuliya <lazynintel@gmail.com>
2021-06-19 23:11:20 +0530

    made lock_unlock.s routines as functions

--- src/cortos/cortos/files/lock_unlock.s
+++ src/cortos/cortos/files/lock_unlock.s
@@ -1,5 +1,5 @@
 ! see the reference file `cortos/res/lock_unlock.s`
-! TODO: handle calling covention
+! TODO: test the locks
 
 ! Usage Note:
 ! ajit_func_sync_lock_get(<index: an-integer-index>);
@@ -8,28 +8,39 @@
 
 ajit_func_sync_lock_get:
   ! i0 contains an index to the correct locking variable
+  save  %sp, -96, %sp       ! func prefix
+
   set SIMPLE_SYNC_VARS_BASE, %l0
   sll i0, 0x2, i0                   ! * 4
 retry_sync_lock:
-  ldstub [%l0 + %i0], %l0
+  ldstub [%l0+%i0], %l0
   tst %l0
   be out
   nop
 sync_lock_loop:
-  ldub [%l0 + %i0], %l0
+  ldub [%l0+%i0], %l0
   tst %l0
   bne sync_lock_loop
   nop
   ba,a retry_sync_lock
 out:
-  nop
+
+  restore                   ! func suffix
+  jmp %o7+8                 ! func suffix
+  nop                       ! func suffix
 
 
 ajit_func_sync_lock_release:
   ! i0 contains an index to the correct locking variable
+  save  %sp, -96, %sp       ! func prefix
+
   set SIMPLE_SYNC_VARS_BASE, %l0
   sll i0, 0x2, i0                   ! * 4
 
   stbar
-  stub %g0, [%l0 + %i0]
+  stub %g0, [%l0+%i0]
+
+  restore                   ! func suffix
+  jmp %o7+8                 ! func suffix
+  nop                       ! func suffix
 
